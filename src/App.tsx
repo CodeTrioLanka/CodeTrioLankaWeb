0a46cc9f12069929219ef05f335570f5ad52c37c
Senuda Damnidu Karunarathne Adihetty <senuadihetty429@gmail.com>
2026-02-03 21:47:27 +0530

    feat: add smooth scrolling with Lenis
    - Install lenis package
    - Create SmoothScroll component
    - Integrate SmoothScroll into App.tsx
    - Add Lenis CSS to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProjectDetails from "./pages/ProjectDetails";
 import NotFound from "./pages/NotFound";
+import SmoothScroll from "./components/SmoothScroll";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <SmoothScroll />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
